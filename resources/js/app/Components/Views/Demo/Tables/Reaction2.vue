d4f93ddb9c900f94dfa73ce392ec3c61b55b3246
ThanhTV <tranthanh28121993@gmail.com>
2023-09-12 14:47:59 +0700

    update view

--- resources/js/app/Components/Views/Demo/Tables/Reaction2.vue
+++ resources/js/app/Components/Views/Demo/Tables/Reaction2.vue
@@ -46,19 +46,11 @@
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination
-        background
-        layout="prev, pager, next"
-        @current-change="handleCurrentChange"
-        :current-page.sync="currentPage"
-        :page-size="perPage"
-        :total="totalPages">
-    </el-pagination>
 
 
     <el-dialog title="Reaction" :visible.sync="dialogFormVisible">
       <el-form :model="form">
-        <el-form-item label="name" :label-width="formLabelWidth">
+        <el-form-item label="Name" :label-width="formLabelWidth">
           <el-input v-model="form.name" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="File" :label-width="formLabelWidth">
@@ -69,13 +61,13 @@
               ref="upload"
               :on-change="handleImport"
               :auto-upload="false">
-            <el-button slot="trigger" size="small" type="primary">select file</el-button>
+            <el-button slot="trigger" size="small" type="primary">Select file</el-button>
           </el-upload>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
     <el-button @click="dialogFormVisible = false">Cancel</el-button>
-    <el-button type="primary" @click="handleAddReaction">submit</el-button>
+    <el-button type="primary" @click="handleAddReaction">Submit</el-button>
   </span>
     </el-dialog>
   </div>
@@ -90,24 +82,6 @@ export default {
       perPage: 10,
       currentPage: 1,
       search: '',
-
-      gridData: [{
-        date: '2016-05-02',
-        name: 'John Smith',
-        address: 'No.1518,  Jinshajiang Road, Putuo District'
-      }, {
-        date: '2016-05-04',
-        name: 'John Smith',
-        address: 'No.1518,  Jinshajiang Road, Putuo District'
-      }, {
-        date: '2016-05-01',
-        name: 'John Smith',
-        address: 'No.1518,  Jinshajiang Road, Putuo District'
-      }, {
-        date: '2016-05-03',
-        name: 'John Smith',
-        address: 'No.1518,  Jinshajiang Road, Putuo District'
-      }],
       dialogTableVisible: false,
       dialogFormVisible: false,
       form: {
@@ -180,33 +154,9 @@ export default {
         });
       })
     },
-    handleEditUser() {
+    getListReactions() {
       this.startLoading()
-      let dataEdit = {
-        status: this.form.status ? 1 : 0
-      }
-      axios.put(`/api/adm/${this.form.id}`, dataEdit).then(response => {
-        this.stopLoading()
-        this.currentUser.status = dataEdit.status
-        this.dialogFormVisible = false
-        this.$notify({
-          title: 'Success',
-          message: 'update successfully',
-          type: 'success'
-        });
-      }).catch(error => {
-        this.stopLoading()
-        this.dialogFormVisible = false
-        this.handleErrorNotPermission(error)
-      })
-    },
-    handleCurrentChange(val) {
-      this.$router.push({path: '/manage/users', query: {page: val, status: this.currentTab}});
-      this.getListReactions(val, this.currentTab)
-    },
-    getListReactions(page = 1) {
-      this.startLoading()
-      axios.get(`/api/reaction/?page=${page}`).then((response) => {
+      axios.get(`/api/reaction`).then((response) => {
         this.stopLoading()
         this.data = response.data.data
         // this.totalPages = response.data.data.total
