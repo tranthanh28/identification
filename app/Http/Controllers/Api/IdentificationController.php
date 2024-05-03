58b2e046311e741c1d5cb8da7dbd5c996b77b5d4
TranDuc1908 <duc.php.unity@gmail.com>
2024-05-03 17:02:00 +0700

    init branch backend

--- app/Http/Controllers/Api/IdentificationController.php
+++ app/Http/Controllers/Api/IdentificationController.php
@@ -33,78 +33,12 @@ class IdentificationController extends Controller
     }
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required',
-            'content_file' => 'required|string',
-            'pass_day' => 'required|numeric',
-        ]);
-        $keywords = preg_split("/\r\n|\n|\r/", $request->content_file);
-        $date = Carbon::now()->format("Y_m_d_H_i_s");
-        $fileName = $request->name . "_" . $date .'.xlsx';
-        $data = [
-            'name' => $request->name,
-            'content_file' => json_encode($keywords),
-            'file_name' => $fileName,
-            'pass_day' => $request->pass_day,
-        ];
 
-        $scanGroup = ScanGroup::create($data);
-
-        foreach ($keywords as $keyword) {
-            ScanGroupJob::dispatch([$keyword], $fileName, $scanGroup, $request->name);
-        }
-
-        return response()->json([
-            'status' => true,
-            'message' => 'created successfully',
-            'data' => $scanGroup
-        ], 200);
     }
 
     public function update(Request $request, $id)
     {
-        $this->validate($request, [
-            'name' => 'required',
-            'content_file' => 'required|string',
-            'pass_day' => 'required|numeric',
-        ]);
-
-        $scanGroup = ScanGroup::findorFail($id);
-        if ($scanGroup->status != 2) {
-            return response()->json([
-                'status' => false,
-                'message' => 'Update failed (status == 1|0)'
-            ], 400);
-        }
-
-        $keywords = preg_split("/\r\n|\n|\r/", $request->content_file);
-        $date = Carbon::now()->format("Y_m_d_H_i_s");
-        $fileName = $request->name . "_" . $date .'.xlsx';
-        $data = [
-            'name' => $request->name,
-            'content_file' => json_encode($keywords),
-            'file_name' => $fileName,
-            'pass_day' => $request->pass_day,
-            'status' => 0,
-        ];
-
-        $scanGroup->update($data);
-
-        ScanGroupJob::dispatch($keywords, $fileName, $scanGroup);
-
-        return response()->json([
-            'status' => true,
-            'message' => 'update successfully',
-            'data' => $scanGroup
-        ], 200);
 
     }
 
-    public function exportExcel(Request $request)
-    {
-        $this->validate($request, [
-            'file_name' => 'required'
-        ]);
-        return Storage::download('/groups_xlsx/' . $request->file_name);
-    }
 }
